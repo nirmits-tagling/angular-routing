b6a2d9ab4204b4be12ef4068c98e68b933671095
nirmits-tagline <nirmits.tagline@gmail.com>
2022-12-29 16:52:56 +0530

    Impement Some Css

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  // @Input()
+   headernavcontent:any;
+
+  btnHeaderType:string ="button"
+  getquote:string = "Get Quote"
+  taglinelogo:string = "../assets/../assets/images/tagline.png"
+  headernav:string[]=["Case Study","About Us","Technology","Industry"]
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
